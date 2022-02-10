6e595d3b8f21051f30756d8db1a7fc2ff66796d9
ba066727 <ba066727@gmail.com>
2022-02-10 20:18:09 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./Search";
+
+import "../styles/Header.css";
+
+export default function Header({ searchCity }) {
+  return (
+    <header className="Header">
+      <h1 className="Header__title"> موقع زياد سمانة للطقس </h1>
+      <Search searchCity={searchCity} />
+    </header>
+  );
+}
